b272a52a16bdc6c158fc2d7c3230a3a690925c83
Marlene Koh <marlenekohxml@gmail.com>
2018-03-14 22:25:26 +0800

    Update junit test case for security util

--- src/test/java/seedu/address/storage/SecurityUtilTest.java
+++ src/test/java/seedu/address/storage/SecurityUtilTest.java
@@ -1,5 +1,29 @@
 package seedu.address.storage;
 
-public class SecurityUtilTest {
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+import org.junit.Test;
+
+public class SecurityUtilTest {
+    @Test
+    public void encryptDecrypt_successful_emptyResult() throws Exception {
+        File testFile = new File("./src/test/data/sandbox/temp.xml");
+        PrintWriter writer = new PrintWriter(testFile);
+        String test = new String("testing encryption and decryption");
+
+        writer.write(test);
+        writer.close();
+
+        SecurityUtil.encrypt(testFile);
+        SecurityUtil.decrypt(testFile);
+
+        BufferedReader br = new BufferedReader(new FileReader(testFile));
+        String stuff = br.readLine();
+        assertEquals(stuff, test);
+    }
 }
