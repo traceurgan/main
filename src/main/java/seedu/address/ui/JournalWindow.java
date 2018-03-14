3eef20187c42e74c1e5e29b4dc2372e539014aa4
tracegan <traceur.gan@gmail.com>
2018-03-14 21:23:16 +0800

    Allow typing in JournalWindow

--- src/main/java/seedu/address/ui/JournalWindow.java
+++ src/main/java/seedu/address/ui/JournalWindow.java
@@ -3,6 +3,10 @@ package seedu.address.ui;
 import javax.swing.*;
 
 public class JournalWindow extends JFrame {
+    public JournalWindow() {
+        final JTextArea textArea = new JTextArea();
+        getContentPane().add(textArea);
+    }
     //do something
 
 }
