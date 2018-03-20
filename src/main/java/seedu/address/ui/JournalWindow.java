d8a27bc92eeb6e0033d98b5251b5869195fa528e
tracegan <traceur.gan@gmail.com>
2018-03-20 21:07:02 +0800

    Changed implementation to use javafx instead of swing

--- src/main/java/seedu/address/ui/JournalWindow.java
+++ src/main/java/seedu/address/ui/JournalWindow.java
@@ -1,16 +1,35 @@
 package seedu.address.ui;
 
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Controller for a journal page
  */
-public class JournalWindow extends JFrame {
-    public JournalWindow() {
-        final JTextArea textArea = new JTextArea();
-        getContentPane().add(textArea);
-    }
-    //do something
+//public class JournalWindow extends UiPart<Stage> {
+public class JournalWindow {
 
+    public void makeBox (Stage stage) {
+        AnchorPane ap = new AnchorPane();
+
+        TextArea journalTextArea = new TextArea();
+        AnchorPane.setLeftAnchor(journalTextArea, 0.0);
+        AnchorPane.setTopAnchor(journalTextArea, 0.0);
+        AnchorPane.setRightAnchor(journalTextArea, 0.0);
+        AnchorPane.setBottomAnchor(journalTextArea, 0.0);
+
+        ap.getChildren().add(journalTextArea);
+        Scene scene = new Scene(ap);
+
+        stage.setScene(scene);
+        stage.setTitle("Journal");
+        stage.show();
+    }
+
+//    public void show() {
+//        getRoot().show();
+//    }
 }
