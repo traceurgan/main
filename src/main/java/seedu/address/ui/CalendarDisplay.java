141aea49b6f3144677e15a9cddc31381917ef98c
DuaKingKong <crimson_sabre@hotmail.com>
2018-03-22 02:29:54 +0800

    bug fix#5

--- src/main/java/seedu/address/ui/CalendarDisplay.java
+++ src/main/java/seedu/address/ui/CalendarDisplay.java
@@ -1,13 +1,13 @@
 package seedu.address.ui;
 
-
+import java.util.logging.Logger;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
-
+import seedu.address.commons.core.LogsCenter;
 
 
 /**
@@ -15,6 +15,7 @@ import javafx.scene.layout.Region;
  */
 public class CalendarDisplay extends UiPart<Region> {
 
+    private static final Logger logger = LogsCenter.getLogger(CalendarDisplay.class);
     private static final String FXML = "CalendarDisplay.fxml";
 
     private final StringProperty displayed = new SimpleStringProperty("");
