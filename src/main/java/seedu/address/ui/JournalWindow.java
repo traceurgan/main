ad481cef2438743bb70e716409dd04ea9967db40
tracegan <traceur.gan@gmail.com>
2018-03-21 18:47:24 +0800

    CheckStyle edits

--- src/main/java/seedu/address/ui/JournalWindow.java
+++ src/main/java/seedu/address/ui/JournalWindow.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
-
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
@@ -34,29 +32,18 @@ public class JournalWindow extends UiPart<Stage> {
     }
 
     private void setAnchor(TextArea journalTextArea) {
-        AnchorPane.setTopAnchor(journalTextArea,0.0);
-        AnchorPane.setBottomAnchor(journalTextArea,0.0);
-        AnchorPane.setLeftAnchor(journalTextArea,0.0);
-        AnchorPane.setRightAnchor(journalTextArea,0.0);
-    }
-
-    public void makeBox (Stage stage) {
-        AnchorPane ap = new AnchorPane();
-
-        TextArea journalTextArea = new TextArea();
-        AnchorPane.setLeftAnchor(journalTextArea, 0.0);
         AnchorPane.setTopAnchor(journalTextArea, 0.0);
-        AnchorPane.setRightAnchor(journalTextArea, 0.0);
         AnchorPane.setBottomAnchor(journalTextArea, 0.0);
-
-        ap.getChildren().add(journalTextArea);
-        Scene scene = new Scene(ap);
-
-        stage.setScene(scene);
-        stage.setTitle("Journal");
+        AnchorPane.setLeftAnchor(journalTextArea, 0.0);
+        AnchorPane.setRightAnchor(journalTextArea, 0.0);
     }
+
 
     public void show() {
         getRoot().show();
+    }
+
+    public boolean isShowing() {
+        return getRoot().isShowing();
     }
 }
