5926a4096be71c5ad805c3cb4a651fa09913428f
tracegan <traceur.gan@gmail.com>
2018-03-24 00:40:44 +0800

    Shifted responsibility of construction and management of ui elements to the fxml file rather than JournalWindow.java

--- src/main/java/seedu/address/ui/JournalWindow.java
+++ src/main/java/seedu/address/ui/JournalWindow.java
@@ -1,13 +1,7 @@
 package seedu.address.ui;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.Month;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -18,20 +12,17 @@ public class JournalWindow extends UiPart<Stage> {
 
     private static final String FXML = "JournalWindow.fxml";
 
-    @FXML
-    private AnchorPane journalTextPlaceholder;
-
-    public JournalWindow (Stage root) {
+    private JournalWindow (Stage root) {
         super (FXML, root);
-
-        TextArea journalTextArea = new TextArea();
-        setAnchor(journalTextArea);
-        journalTextPlaceholder.getChildren().add(journalTextArea);
 
         String date = concatenateDate();
 
         root.setTitle(date + " - Journal");
         root.initModality(Modality.APPLICATION_MODAL);
+    }
+
+    public JournalWindow () {
+        this(new Stage());
     }
 
     private String concatenateDate() {
@@ -41,18 +32,6 @@ public class JournalWindow extends UiPart<Stage> {
         int yyyy = currentDate.getYear();
         return String.format("%04d", yyyy) + String.format("%02d", mm) + String.format("%02d", dd);
     }
-
-    public JournalWindow () {
-        this(new Stage());
-    }
-
-    private void setAnchor(TextArea journalTextArea) {
-        AnchorPane.setTopAnchor(journalTextArea, 0.0);
-        AnchorPane.setBottomAnchor(journalTextArea, 0.0);
-        AnchorPane.setLeftAnchor(journalTextArea, 0.0);
-        AnchorPane.setRightAnchor(journalTextArea, 0.0);
-    }
-
 
     public void show() {
         getRoot().show();
