b1e0cdde103b4d830825612a41c3b81e27581eec
Marlene Koh <marlenekohxml@gmail.com>
2018-03-23 23:06:09 +0800

    Add method assertDoesNotThrow() in Assert.java

--- src/test/java/seedu/address/testutil/Assert.java
+++ src/test/java/seedu/address/testutil/Assert.java
@@ -52,4 +52,16 @@ public class Assert {
         void call() throws Exception;
     }
 
+    /**
+     * Asserts that the {@code callable} does not throw any exception.
+     */
+    public static void assertDoesNotThrow(VoidCallable callable){
+        try{
+            callable.call();
+        } catch (Throwable unexpectedException){
+            String errorMessage = String.format(
+                    "Expected nothing thrown, however %s thrown", unexpectedException.getMessage());
+            throw new AssertionFailedError(errorMessage);
+        }
+    }
 }
