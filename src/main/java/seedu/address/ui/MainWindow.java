9be971a9108548ce38280a3fd370c6595dd03f11
Marlene Koh <marlenekohxml@gmail.com>
2018-04-05 04:23:08 +0800

    Collate

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -17,6 +17,7 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+import seedu.address.commons.events.ui.ShowJournalWindowRequestEvent;
 import seedu.address.logic.Logic;
 import seedu.address.model.UserPrefs;
 
