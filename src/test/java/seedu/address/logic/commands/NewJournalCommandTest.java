4fb783a1ba505b8833ad6dd1174c50842c985c47
tracegan <traceur.gan@gmail.com>
2018-04-05 02:47:13 +0800

    CS

--- src/test/java/seedu/address/logic/commands/NewJournalCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NewJournalCommandTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
-import static seedu.address.logic.commands.NewJournal.NEW_JOURNAL_ENTRY_CREATED;
+import static seedu.address.logic.commands.NewJournalCommand.NEW_JOURNAL_ENTRY_CREATED;
 
 import org.junit.Test;
 
@@ -9,7 +9,7 @@ public class NewJournalCommandTest {
 
     @Test
     public void execute_newjournal_success() {
-        CommandResult result = new NewJournal().execute();
+        CommandResult result = new NewJournalCommand().execute();
         assertEquals(NEW_JOURNAL_ENTRY_CREATED, result.feedbackToUser);
     }
 }
