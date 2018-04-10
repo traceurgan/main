25cc769765475d91681bddcda58b1feb44464aad
traceurgan <traceur.gan@gmail.com>
2018-04-10 20:00:14 +0800

    Added marlenes new files

---
+++ src/main/java/seedu/address/model/person/timetable/TimetableComparatorUtil.java
@@ -0,0 +1,16 @@
+package seedu.address.model.person.timetable;
+
+//@@author marlenekoh
+/**
+ * A class containing utility methods for comparing two timetables
+ */
+public class TimetableComparatorUtil {
+
+    public static void compareTimetable(Timetable first, String second) {
+        compareTimetable(first, new Timetable(second));
+    }
+
+    public static void compareTimetable(Timetable first, Timetable second) {
+
+    }
+}
