956f75c669de609d2fc0f4f62688fcf8869ed510
traceurgan <traceur.gan@gmail.com>
2018-04-15 20:19:04 +0800

    FIx test

--- src/test/java/seedu/address/logic/commands/NewJournalCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NewJournalCommandTest.java
@@ -3,10 +3,16 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.NewJournalCommand.NEW_JOURNAL_ENTRY_CREATED;
 
+import org.junit.Rule;
 import org.junit.Test;
+
+import seedu.address.ui.testutil.EventsCollectorRule;
 
 //@@author traceurgan
 public class NewJournalCommandTest {
+
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     @Test
     public void execute_newjournal_success() {
